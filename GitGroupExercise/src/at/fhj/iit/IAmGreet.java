2afb33263601c00ad18048f6e41ba79385e8a1a2
flagg <andreas.kraus@edu.fh-joanneum.at>
2018-05-12 14:34:17 +0200

    nimma rot

---
+++ GitGroupExercise/src/at/fhj/iit/IAmGreet.java
@@ -0,0 +1,19 @@
+package at.fhj.iit;
+
+public class IAmGreet {
+   private String name = null;
+
+   public IAmGreet (String name){
+       this.name = name;
+
+   }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
